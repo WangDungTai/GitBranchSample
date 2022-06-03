2193f7f8bc042469064e014822a5e60aa2103352
LinDT <wangdungtai@gmail.com>
2022-06-03 09:59:56 +0800

    1+ version

--- GuitHubBranchSample/src/GitBackApp/MainApp.java
+++ GuitHubBranchSample/src/GitBackApp/MainApp.java
@@ -5,7 +5,16 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 5));
 
 	}
 
+}
+
+class Math {
+	int add(int a, int b) {
+		return a+b;
+	}
 }
